4ba3782e96b6cdf99b56acc7676c7eb266c49210
Luca Giuliani <giulianil@microsoft.com>
2025-03-12 20:47:34 +0100

    Enhance resource management for myservice; add resource limits and requests in YAML configuration

--- Generate-AllIngresses.ps1
+++ Generate-AllIngresses.ps1
@@ -1,4 +1,4 @@
 helm install --dry-run --generate-name -f .\myvalues.yaml .\multidomain-ingress > k8s\all-ingresses.yaml
 $yamls = Get-Content k8s\all-ingresses.yaml | ConvertFrom-Yaml -AllDocuments
-$yamls = $yamls[1..$yamls.Count]
+$yamls = $yamls[1..$yamls.Count]  # Removes the first YAML, which is the Helm template manifest
 $yamls | ConvertTo-Yaml | Set-Content k8s\all-ingresses.yaml -Encoding ascii
